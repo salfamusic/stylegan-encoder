e3f8ee9c35ea66235329e7345fbd82d7b3ce9ac0
Michael King <salfa.music@gmail.com>
2020-07-07 09:39:35 -0500

    yup

--- encode_images.py
+++ encode_images.py
@@ -37,7 +37,7 @@ def main():
     parser.add_argument('--mask_dir', default='masks', help='Directory for storing optional masks')
     parser.add_argument('--load_last', default='', help='Start with embeddings from directory')
     parser.add_argument('--dlatent_avg', default='', help='Use dlatent from file specified here for truncation instead of dlatent_avg from Gs')
-    parser.add_argument('--model_url', default='https://drive.google.com/uc?id=1nT_cf610q5mxD_jACvV43w4SYBxsPUBq', help='Fetch a StyleGAN model to train on from this URL') # karras2019stylegan-ffhq-1024x1024.pkl
+    parser.add_argument('--model_url', default='https://drive.google.com/uc?id=1IxRyfTf62KBjyc486JA5tGLVnFh_d4eO', help='Fetch a StyleGAN model to train on from this URL') # karras2019stylegan-ffhq-1024x1024.pkl
     parser.add_argument('--model_scale', default=14, help='The dimension of images in the StyleGAN model', type=int)
     parser.add_argument('--batch_size', default=1, help='Batch size for generator and perceptual model', type=int)
     parser.add_argument('--optimizer', default='adam', help='Optimization algorithm used for optimizing dlatents')
